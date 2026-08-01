5c30bcd679188895d83c30bbeefebf7c1402e0be
Mubarak Sadoon <msadoon@gmail.com>
2026-07-31 22:41:58 -0400

    Fixed the navigation stack gap from status bar issue.
    Recreated entire project with a new template. Tests passing, running on iOS 18 and 26 simulators.

---
+++ TopStock/TopstockTests/TopStockNetworkingTests.swift
@@ -0,0 +1,49 @@
+import Testing
+import Foundation
+@testable import Topstock
+
+struct TopStockNetworkingTests {
+    private let networking = MockTopStockNetworking()
+    
+    @Test func moversStocks() async throws {
+        self.networking.expectedState = .moversStocks
+        
+        let movers: Movers = try await networking.retrieveData(for: .moversStocks)
+        
+        #expect(movers.gainers.count == 10)
+        #expect(movers.losers.count == 10)
+        
+        let sampleSecurity = movers.losers[3]
+        
+        #expect(sampleSecurity.price == 0.0986)
+        #expect(sampleSecurity.symbol == "VSTD")
+        #expect(sampleSecurity.percentChange == -45.22)
+        #expect(sampleSecurity.change == -0.0814)
+    }
+    
+    @Test func historicalBars() async throws {
+        self.networking.expectedState = .historicalBars
+        
+        let historicalBars: HistoricalBars = try await networking.retrieveData(for: .historicalBars("AAPL", .OneHour, "2026-02-26T09:00:00Z"))
+        
+        #expect(historicalBars.bars.count == 16)
+        
+        let sampleCandleStickBar = historicalBars.bars[3]
+        
+        #expect(sampleCandleStickBar.numTrades == 213)
+        #expect(sampleCandleStickBar.volume == 9240)
+        #expect(sampleCandleStickBar.highPrice == 375.01)
+        let timestampDate = try #require(sampleCandleStickBar.timestamp)
+        let dateComponents = Calendar.current.dateComponents([.hour, .day, .month, .year], from: timestampDate)
+        /** FIXME: Ensure we have a Utility to extract the 24 hour time.
+        #expect(dateComponents.hour == 11)
+         */
+        #expect(dateComponents.day == 26)
+        #expect(dateComponents.month == 2)
+        #expect(dateComponents.year == 2026)
+        #expect(sampleCandleStickBar.closePrice == 374.74)
+        #expect(sampleCandleStickBar.openPrice == 374.9)
+        #expect(sampleCandleStickBar.lowPrice == 374.7)
+        #expect(sampleCandleStickBar.volumeWeightedAveragePrice == 374.82)
+    }
+}
---
+++ TopStock/TopstockTests/UtilitiesTests.swift
@@ -0,0 +1,25 @@
+import Testing
+import Foundation
+@testable import Topstock
+
+struct UtilitiesTests {
+    static private let utcDate = "2025-12-19T23:02:25Z"
+    static private let nonUTCDate = "1969-12-31A23:59:59Z"
+    static private let exampleFilename = "Movers"
+    
+    @Test("Successful UTC datetime to UTC date conversion.", arguments: [utcDate]) func successfulDateConversionToUTC(datetime: String) async throws {
+        let conversionResultDate = try #require(Utilities.validUTCDate(from: datetime))
+        
+        let conversionResultDateComponents = Calendar.current.dateComponents([.day, .month, .year], from: conversionResultDate)
+        
+        #expect(conversionResultDateComponents.day == 19)
+        #expect(conversionResultDateComponents.month == 12)
+        #expect(conversionResultDateComponents.year == 2025)
+    }
+    
+    @Test("Failure non-UTC datetime to UTC conversion.", arguments: [nonUTCDate]) func failureDateConversionToUTC(datetime: String) async {
+        let conversionResult = Utilities.validUTCDate(from: datetime)
+        
+        #expect(conversionResult == nil)
+    }
+}
